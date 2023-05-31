cc2e3b15f61781a5ba81373c729cd1ffe6461f5c
techmania-art <134226958+techmania-art@users.noreply.github.com>
2023-05-31 12:32:21 +0530

    Update 5_lists_exercise.py
    just had to add del with the list<heros>for the syntax to work

--- Basics/Exercise/5_lists/5_lists_exercise.py
+++ Basics/Exercise/5_lists/5_lists_exercise.py
@@ -54,7 +54,7 @@ print(heros)
 # 4. Now you don't like thor and hulk because they get angry easily :)
 #    So you want to remove thor and hulk from list and replace them with doctor strange (because he is cool).
 #    Do that with one line of code.
-heros[1:3]=['doctor strange']
+del heros[1:3]=['doctor strange']
 print(heros)
 # 5. Sort the list in alphabetical order
 heros.sort()
